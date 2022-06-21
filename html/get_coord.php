c91c9e27d5ab4198d04b95b8821f2bd07c458849
shulandt <shulandt.aleksandr@gmail.com>
2022-06-21 17:49:59 +0300

    fix

--- html/get_coord.php
+++ html/get_coord.php
@@ -1,11 +1,12 @@
 <?php
 
 $ctrlname = "backend_data/from_client";
-$content = "empty";
+$content = 'empty';
 
 function readServInfo()
 {
 	global $ctrlname;
+	global $content;
 	if(!empty($_POST)) {
 		if(isset($_POST['num'])) {
 			$num = $_POST['num'];
